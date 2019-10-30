6dfabe5b15e32f180b4380339142548495949592
Okyfunoky <okyfunoky@hotmail.com>
2019-10-30 14:48:06 -0700

    Initial population

---
+++ src/server.ts
@@ -0,0 +1,16 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Define middleware here
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Connect to the Mongo DB
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist");
+
+// Start the API server
+app.listen(PORT, function() {
+  console.log(`http://localhost:${PORT}!`);
+});
